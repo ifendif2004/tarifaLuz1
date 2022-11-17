f7b8a3c641d0081e42e3d528d215f924015ad5cb
ifendif2004 <ifendif2004@gmail.com>
2022-11-17 17:15:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,103 +1,221 @@
-var dateControl = document.querySelector('input[type="date"]')
-var geolimit = document.getElementById('sellimit')
-var lista = document.getElementById('lista')
-var btnConsultar = document.getElementById('btnConsultar')
+const dateControl = document.querySelector('input[type="date"]')
+const geolimit = document.getElementById('sellimit')
+const lista = document.getElementById('lista')
+const btnConsultar = document.getElementById('btnConsultar')
+const buscar = document.getElementById('buscar')
+const calcular = document.getElementById('calcular')
+const contenidobuscar = document.getElementById('contenidobuscar')
+const contenidocalcular = document.getElementById('contenido-calcular')
+const inputkwh = document.getElementById('input-kwh')
+const btncalcularkwh = document.getElementById('calcular-kwh')
+const resultadoenergia = document.getElementById('resultado-energia')
+// const literaleuroshora = document.getElementById('literal-euros-hora')
+const resultadocalculo = document.getElementById('resultado-calculo')
 
-var fecha = new Date();
+
+
+
+// -----------Registrar el Service Worker------------------
+let swLocation = "./swtarifaluz.js";
+if (navigator.serviceWorker) {
+	navigator.serviceWorker.register(swLocation);
+} else {
+	console.log("no se ha podido registrar el SW " + navigator.serviceWorker)
+}
+
+let todoPrecios = [];
+let precios = [];
+resultadocalculo.classList.add("ocultar")
+resultadocalculo.classList.remove("mostrar")
+activarCalcular(precios)
+
+const fecha = new Date();
 dateControl.value = fecha.toJSON().slice(0, 10);
 
 geolimit.addEventListener("click", (event) => {
 	event.preventDefault();
 	lista.innerHTML = '';
+	maxmin.innerHTML = '';
 });
 dateControl.addEventListener("click", (event) => {
 	lista.innerHTML = '';
+	maxmin.innerHTML = '';
 });
 
 btnConsultar.addEventListener("click", (event) => {
 	event.preventDefault();
-	// console.log('click boton xxx');
-	// console.log(dateControl.value);
 	const startdate = dateControl.value + "T00:00";
 	const enddate = dateControl.value + "T23:59";
 	cargarPrecios(startdate, enddate);
 });
 
 
-
 const cargarPrecios = async (startdate, enddate) => {
 	try {
+		btnConsultar.setAttribute("disabled", "")
+		btnConsultar.setAttribute("aria-busy", "true")
+		mostrarLoading();
 		let imagen = '';
 		lista.innerHTML = ''
-		// console.log(geolimit.value)
-		// console.log(startdate)
-		// console.log(enddate)
+
 		const apirest = `https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date=${startdate}&end_date=${enddate}&geo_limit=${geolimit.value}&time_trunc=hour`
-
-		// console.log(apirest)
-
-
 		const respuesta = await fetch(apirest);
-
-		// console.log(respuesta.status);
-		//		console.log(respuesta);
-
-		// Si la respuesta es correcta
+		ocultarLoading();
+		btnConsultar.removeAttribute('disabled')
+		btnConsultar.removeAttribute('aria-busy')
 		if (respuesta.status === 200) {
 			const datos = await respuesta.json();
-			let precios = [];
+			todoPrecios = datos
+			precios = [];
 			datos.included[0].attributes.values.forEach(precio => {
 				precios.push((precio.value / 1000).toFixed(5));
 			});
-			// console.log(precios)
-			let minimo = Math.min(...precios) + 0.02;
-			let maximo = minimo + 0.04;
-			// console.log(minimo)
-
-			//  console.log(datos);
-			// console.log(datos.included[0].attributes.values[0].datetime + ": " + datos.included[0].attributes.values[0].value);
+			let min = Math.min(...precios);
+			let max = Math.max(...precios);
+			let med = precios.reduce((a, b) => a + parseFloat(b, 10), 0)
+			med = med / 24;
+			med = (Math.round(med * 1000000) / 1000000).toPrecision(5)
+			let cuarto = (+med + min) / 2;
+			let minimoMaximo = `
+			<div id="minimo" class="minimo"><p>MÍNIMO</p> <p>${min} €/kwh</p></div>
+			<div id="medio" class="medio"><p>MEDIA</p> <p>${med} €/kwh</p></div>
+			<div id="maximo" class="maximo"><p>MÁXIMO</p> <p>${max} €/kwh</p></div>`;
+			maxmin.innerHTML = minimoMaximo;
 			let preciosHora = '';
-			//			const datos1 = await datos.included[0].json();
-			// console.log(datos.included[0].attributes.values);
-
-
-
-
+			let colorhora = ''
+			let horanext = ''
 			datos.included[0].attributes.values.forEach(hora => {
-				//				console.log(hora.datetime.slice(11,16))
 				let valor = (hora.value / 1000).toFixed(5);
-				if (valor <= minimo) {
-					imagen = './img/verde.png'
-				} else if (valor <= maximo) {
-					imagen = './img/naranja.png'
+				if (valor < cuarto) {
+					colorhora = 'minimo'
+					imagen = './img/cuadradoVerde.png'
+				} else if (valor < med) {
+					colorhora = 'medio'
+					imagen = './img/cuadradoNaranja.png'
 				} else {
-					imagen = './img/rojo.png'
+					colorhora = 'maximo'
+					imagen = './img/puntorojo.png'
 				}
+				horanext = +hora.datetime.slice(11, 13) + 1
+				if ((String(horanext).length) == 1) {
+					horanext = '0' + horanext
+				}
+
 				preciosHora += `
-			<div class="itempreciohora">
-				<img src="${imagen}">
-				<span> ${hora.datetime.slice(11, 16)} -- ${(hora.value / 1000).toFixed(5)} €/kWh </span>
-			</div>`;
+				<div class="itempreciohora">
+					<img src="${imagen}">
+					<span1> ${hora.datetime.slice(11, 13)}h-${horanext}h: <span class="${colorhora}">  ${(hora.value / 1000).toFixed(5)} €/kWh </span></span1>
+				</div>`;
 			});
 
 			document.getElementById('lista').innerHTML = preciosHora;
 
 		} else if (respuesta.status === 401) {
-			console.log('Pusiste la llave mal');
+			ocultarLoading();
+			lista.innerHTML = 'Precios no encontrados';
 		} else if (respuesta.status === 404) {
-			console.log('Precios no encontrados');
+			ocultarLoading();
+			lista.innerHTML = 'Precios no encontrados';
 		} else if (respuesta.status === 502) {
+			ocultarLoading();
 			lista.innerHTML = 'No hay datos para los filtros seleccionados.'
-			console.log('No hay datos para los filtros seleccionados.');
 		} else {
-
-			console.log('Hubo un error y no sabemos que paso');
+			ocultarLoading();
+			lista.innerHTML = 'Hubo un error y no sabemos que paso';
 		}
 
 	} catch (error) {
+		ocultarLoading();
+		lista.innerHTML = 'Hubo un error y no sabemos que paso';
 		console.log(error);
 	}
 
 }
+function ocultarLoading() {
+	document.getElementById("loading").style.display = "none";
+}
+function mostrarLoading() {
+	document.getElementById("loading").style.display = "block";
+}
 
-//cargarPrecios();
+// ---------- calcular --------------
+inputkwh.addEventListener("click", (evt) => {
+	resultadocalculo.classList.add("ocultar")
+	resultadocalculo.classList.remove("mostrar")
+})
+btncalcularkwh.addEventListener("click", (evt) => {
+	resultadocalculo.classList.add("mostrar")
+	resultadocalculo.classList.remove("ocultar")
+	let resultado = ""
+	let preciohora = 0
+	// literaleuroshora.innerText = "Precio en Euros por cada hora"
+	precios.forEach((precio) => {
+		preciohora = parseFloat(precio) * parseFloat(inputkwh.value) / 1000
+		// console.log("precio ", parseFloat(precio) , " resultado: ", parseFloat(precio) * parseFloat(inputkwh.value) )
+		resultado += `
+		<div class="eurohora">
+		<span class="dd">  ${(preciohora).toFixed(5)} € </span>
+		</div>`;
+	})
+	resultadoenergia.innerHTML = resultado
+
+	resultado = ""
+	let imagenColor = {"claseColor": '', "imagen": '' }
+	let horanext = ""
+	if ((String(horanext).length) == 1) {
+		horanext = '0' + horanext
+	}
+	todoPrecios.included[0].attributes.values.forEach(precio => {
+		horanext = +precio.datetime.slice(11, 13) + 1
+		preciohora = parseFloat(precio.value / 1000) * parseFloat(inputkwh.value) / 1000
+		if ((String(horanext).length) == 1) {
+			horanext = '0' + horanext
+		}
+		imagenColor = colores(preciohora.toFixed(5), 0.05, 0.2)
+		resultado += `
+		<div class="eurohora">
+		<img src="${colorImagen.imagen}">&nbsp;&nbsp;
+		<span1> ${precio.datetime.slice(11, 13)}h-${horanext}h:&nbsp;<span class="${colorImagen.claseColor}">  ${preciohora.toFixed(5)} € </span></span1>
+		</div>`;
+
+	});
+	resultadoenergia.innerHTML = resultado
+
+})
+
+function colores (valor, nivel1, nivel2){
+	colorImagen = {"claseColor": '', "imagen": '' }
+	if (valor < nivel1) {
+		colorImagen.claseColor = 'minimo'
+		colorImagen.imagen = './img/cuadradoVerde.png'
+	} else if (valor < nivel2) {
+		colorImagen.claseColor = 'medio'
+		colorImagen.imagen = './img/cuadradoNaranja.png'
+	} else {
+		colorImagen.claseColor = 'maximo'
+		colorImagen.imagen = './img/puntorojo.png'
+	}
+
+	return colorImagen
+}
+
+// ---------- pestañas --------------
+buscar.addEventListener("click", (evt) => {
+	activarCalcular(precios)
+})
+
+calcular.addEventListener("click", (evt) => {
+	if (precios.length < 1) return
+
+	contenidobuscar.style.display = "none"
+	buscar.className = buscar.className.replace(" active", "")
+	contenidocalcular.style.display = "block"
+	calcular.className += " active"
+})
+
+function activarCalcular(precios) {
+	contenidocalcular.style.display = "none"
+	calcular.className = calcular.className.replace(" active", "")
+	contenidobuscar.style.display = "block"
+	buscar.className += " active"
+}
